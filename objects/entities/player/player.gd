5a9acca30b79e6e829dcd5488d3f3be4714b3155
fylyp6 <filipmurawko@gmail.com>
2023-12-14 16:46:33 +0100

    Basic player movement

--- objects/entities/player/player.gd
+++ objects/entities/player/player.gd
@@ -1,1 +1,14 @@
 extends Entity
+
+var walk_spd = 100
+
+func _physics_process(delta):
+	var dir_x = -int(Input.is_action_pressed("left")) + int(Input.is_action_pressed("right")) 
+	
+	# Apply horizontal movement
+	velocity.x = walk_spd * dir_x
+	
+	# Apply gravity
+	velocity.y += 90.8 * delta
+	
+	move_and_slide()
